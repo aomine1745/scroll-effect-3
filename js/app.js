364ba839b470335ebde86ce9e59025b84d306e10
aomine <aomine1745@gmail.com>
2020-06-03 12:47:53 -0500

    v1.0

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const scroll = document.querySelector('.curve')
+window.addEventListener('scroll', function () {
+	const value = 1 + window.scrollY/-500
+	scroll.style.transform = `scaleY(${value})`
+})
